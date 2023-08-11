a7ddb5c18fcf90ba8a02fc2a3da7cec366ad1098
mollymaefraser <107052497+mollymaefraser@users.noreply.github.com>
2023-08-11 08:24:30 +0100

    Map addition

---
+++ src/lib/components/Map.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import { onMount, onDestroy } from 'svelte'
+    import { Map } from 'maplibre-gl';
+    import 'maplibre-gl/dist/maplibre-gl.css';
+  
+    let map: Map;
+    let mapContainer: string | HTMLElement;
+  
+    const apiKey = 'U6Q44BrLCIKE3mhBEMv9';
+  
+    onMount(() => {
+      const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+  
+      map = new Map({
+        container: mapContainer,
+        style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${apiKey}`,
+        center: [initialState.lng, initialState.lat],
+        zoom: initialState.zoom
+      });
+  
+    });
+
+  </script>
+  
+  <div class="map-wrap">
+    <a href="https://www.maptiler.com" class="watermark"><img
+      src="https://api.maptiler.com/maps/basic-v2/?key=U6Q44BrLCIKE3mhBEMv9#1.0/42.60066/8.38228" alt="MapTiler logo"/></a>
+    <div class="map" bind:this={mapContainer}></div>
+  </div>
+  
+  <style>
+    .map-wrap {
+      position: relative;
+      width: 100%;
+      height: calc(100vh - 77px); /* calculate height of the screen minus the heading */
+    }
+  
+    .map {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+    }
+  
+    .watermark {
+      position: absolute;
+      left: 10px;
+      bottom: 10px;
+      z-index: 999;
+    }
+  </style>
---
+++ src/routes/find-provider/+page.svelte
@@ -0,0 +1,11 @@
+
+<script>
+    import Map from "$lib/components/Map.svelte";
+
+
+</script>
+
+
+<div class="map">
+    <Map></Map>
+</div>
